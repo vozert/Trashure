ab960539296d9849c610b0ab36dffd642a41bd5f
vozert <novalriski112@gmail.com>
2025-05-19 14:46:43 +0700

    Add leafet icon pckg

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import L from "leaflet";
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+  iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+  shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
